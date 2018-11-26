b1477c442bb03a1e3bfecfe1ec5532c65fbd8ff0
hmscijso <hmscijso@avans.nl>
2018-11-26 15:03:41 +0100

    Create neo_queries.js

---
+++ models/neo_queries.js
@@ -0,0 +1,29 @@
+
+function createUser(session, user) {
+
+}
+
+
+function deleteUser(session, user) {
+
+}
+
+
+function addFriend(session, user) {
+
+}
+
+
+function removeFriend(session, user) {
+
+}
+
+
+
+
+module.exports = {
+    createUser: createUser,
+    deleteUser: deleteUser(),
+    removeFriend: removeFriend(),
+    addFriend: addFriend(),
+};
