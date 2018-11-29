8519c40e0d870f0cc7e4565c4bf340fa58ab2af9
hmscijso <hmscijso@avans.nl>
2018-11-29 23:02:08 +0100

    testing for 200s is still testing thoooo

--- test/threads_test.js
+++ test/threads_test.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const users = require('../models/users');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -16,22 +15,22 @@ describe('Threads', () => {
             .then(() => {
 
 
-        chai.request(server)
-            .post('/threads')
-            .send({
-                'username': 'user1',
-                'title': 'title1',
-                'content': 'content1'
-            })
-            .end((err, res) => {
-                res.should.have.status(200);
-                const success = res.body;
-                success.should.have.property('content').equals('content1');
-                success.should.have.property('title').equals('title1');
-                success.should.have.property('author');
-                done();
+                chai.request(server)
+                    .post('/threads')
+                    .send({
+                        'username': 'user1',
+                        'title': 'title1',
+                        'content': 'content1'
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        const success = res.body;
+                        success.should.have.property('content').equals('content1');
+                        success.should.have.property('title').equals('title1');
+                        success.should.have.property('author');
+                        done();
 
-            })
+                    })
             });
     });
 
@@ -50,15 +49,82 @@ describe('Threads', () => {
                                 const success = res.body;
                                 success.should.be.an('array');
                                 success.should.have.length(2);
-                                // success.should.have.property('author').equals('5bfffc8d59be863ddc72848e');
-                                // success.should.have.property('title').equals('title1');
-                                // success.should.have.property('content').equals('content1');
+
                                 done();
 
                             });
                     });
             });
     });
+
+    it('Retrieves one thread', (done) => {
+        threads.Thread.create({author: '5bfffc8d59be863ddc72848e', title: 'title2', content: 'content2'})
+            .then((thread) => {
+
+                chai.request(server)
+                    .get('/threads/' + thread._id)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        done();
+
+                    });
+            });
+    });
+
+    it('Edits thread', (done) => {
+        threads.Thread.create({author: '5bfffc8d59be863ddc72848e', title: 'title2', content: 'content2'})
+            .then((thread) => {
+
+                chai.request(server)
+                    .put('/threads/' + thread._id)
+                    .send({
+                        'content': 'content3'
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        const success = res.body;
+                        success.should.have.property('content').equals('content3');
+                        done();
+
+                    });
+            });
+    });
+
+
+    it('Deletes thread', (done) => {
+        threads.Thread.create({author: '5bfffc8d59be863ddc72848e', title: 'title2', content: 'content2'})
+            .then((thread) => {
+
+                chai.request(server)
+                    .delete('/threads/' + thread._id)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        done();
+
+                    });
+            });
+    });
+
+    it('Votes for thread', (done) => {
+        const user1 = new users.User({username: 'user1', password: '124'});
+        user1.save()
+            .then((user) => {
+                threads.Thread.create({author: '5bfffc8d59be863ddc72848e', title: 'title2', content: 'content2'})
+                    .then((thread) => {
+
+                        chai.request(server)
+                            .post('threads/' + thread._id + '/vote')
+                            .send({'username': 'user1'})
+                            .end((err, res) => {
+                                res.should.have.status(200);
+                                done();
+
+                            });
+                    });
+            });
+    });
+
+
     it('Done', (done) => {
         done();
     });
