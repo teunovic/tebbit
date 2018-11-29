a158685cde4337e23b5d27e501f1184e8136c374
teunovic <ttfcolin@avans.nl>
2018-11-29 23:53:40 +0100

    formatting

--- controllers/threadscontroller.js
+++ controllers/threadscontroller.js
@@ -6,7 +6,7 @@ const ErrorResponse = require('../response_models/errorresponse');
 function get(req, res) {
     let threadId = req.params.id;
 
-    if(!mongoose.Types.ObjectId.isValid(threadId)) {
+    if (!mongoose.Types.ObjectId.isValid(threadId)) {
         res.status(422).json(new ErrorResponse(1, "Invalid thread id").getResponse());
         return;
     }
@@ -14,11 +14,10 @@ function get(req, res) {
     threads.Thread.findById(threadId)
         .populate("comments")
         .then(thread => {
-            if(!thread) {
+            if (!thread) {
                 res.status(404).json(new ErrorResponse(1, "Could not find thread").getResponse());
                 return;
             }
-            // .toObject({virtuals: true}) om de berekende up/downvotes erbij te doen
             res.status(200).json(thread.toObject());
         })
         .catch(err => {
@@ -44,17 +43,17 @@ function getAll(req, res) {
                 // Zorg dat de virtuals (up/downvotes) erbij worden verstuurd
                 response.push(r.toObject());
             });
-            if(sort == 2) {
+            if (sort == 2) {
                 response.sort((a, b) => {
                     return a.upvotes === b.upvotes ? 0 :
                         a.upvotes < b.upvotes ? 1 : -1;
                 });
-            } else if(sort == 3) {
+            } else if (sort == 3) {
                 response.sort((a, b) => {
                     return a.points === b.points ? 0 :
                         a.points < b.points ? 1 : -1;
                 });
-            } else if(sort == 4) {
+            } else if (sort == 4) {
                 response.sort((a, b) => {
 
                 });
@@ -68,10 +67,10 @@ function getAll(req, res) {
         })
 }
 
-function create(req,res) {
+function create(req, res) {
     users.User.findOne({username: req.body.username, nonActive: false})
         .then(user => {
-            if(!user) {
+            if (!user) {
                 res.status(409).json(new ErrorResponse(1, "Could not find user").getResponse());
                 return;
             }
@@ -90,16 +89,16 @@ function create(req,res) {
         });
 }
 
-function edit(req,res) {
+function edit(req, res) {
     let threadId = req.params.id;
     let newContent = req.body.content;
 
-    if(!mongoose.Types.ObjectId.isValid(threadId)) {
+    if (!mongoose.Types.ObjectId.isValid(threadId)) {
         res.status(422).json(new ErrorResponse(1, "Invalid thread id").getResponse());
         return;
     }
 
-    if(!newContent || newContent.length === 0) {
+    if (!newContent || newContent.length === 0) {
         res.status(409).json(new ErrorResponse(1, "Content can not be empty").getResponse());
         return;
     }
@@ -107,7 +106,7 @@ function edit(req,res) {
     let options = {new: true};
     threads.Thread.findByIdAndUpdate(threadId, {content: newContent}, options)
         .then(thread => {
-            if(!thread) {
+            if (!thread) {
                 res.status(404).send(new ErrorResponse(1, "Could not find thread").getResponse());
                 return;
             }
@@ -122,14 +121,14 @@ function edit(req,res) {
 function remove(req, res) {
     let threadId = req.params.id;
 
-    if(!mongoose.Types.ObjectId.isValid(threadId)) {
+    if (!mongoose.Types.ObjectId.isValid(threadId)) {
         res.status(422).json(new ErrorResponse(1, "Invalid thread id").getResponse());
         return;
     }
 
     threads.Thread.findById(threadId)
         .then(thread => {
-            if(!thread) {
+            if (!thread) {
                 res.status(404).json(new ErrorResponse(1, "Could not find thread").getResponse());
                 return;
             }
@@ -156,27 +155,27 @@ function vote(req, res) {
     let username = req.body.username;
     let upvote = req.body.vote !== "down";
 
-    if(!mongoose.Types.ObjectId.isValid(threadId)) {
+    if (!mongoose.Types.ObjectId.isValid(threadId)) {
         res.status(422).json(new ErrorResponse(1, "Invalid thread id").getResponse());
         return;
     }
 
     threads.Thread.findById(threadId)
         .then(thread => {
-            if(!thread) {
+            if (!thread) {
                 res.status(404).json(new ErrorResponse(1, "Could not find thread").getResponse());
                 return;
             }
             users.User.findOne({username: username})
                 .then(user => {
-                    if(!user) {
+                    if (!user) {
                         res.status(404).json(new ErrorResponse(1, "Could not find user").getResponse());
                         return;
                     }
                     // Remove existing votes by this user
-                    for(i = thread.votes.length - 1; i >= 0; i--) {
+                    for (i = thread.votes.length - 1; i >= 0; i--) {
                         let v = thread.votes[i];
-                        if(v.voter.equals(user._id))
+                        if (v.voter.equals(user._id))
                             thread.votes.splice(i, 1);
                     }
                     let vote = new threads.Vote({voter: user._id, isUpvote: upvote});
